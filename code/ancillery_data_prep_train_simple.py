599391a4c5002b7d9a40c494129db06c3bbb426f
Roland Németh <roland.nemeth000@gmail.com>
2024-05-22 17:52:59 +0200

    Should be the same as the previous, but I guess some files are overwritten.

--- code/ancillery_data_prep_train_simple.py
+++ code/ancillery_data_prep_train_simple.py
@@ -1,12 +1,8 @@
 import os
 import pathlib
 import gc
-import json
 import typing
 import pickle
-import argparse
-import itertools
-from pprint import pprint
 
 import numpy as np
 import pandas as pd
@@ -17,34 +13,50 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.svm import SVC
 
-def load_tabular(path:pathlib.Path) -> typing.Union[pd.DataFrame|pd.Series]:
+
+def load_tabular(path: pathlib.Path) -> typing.Union[pd.DataFrame | pd.Series]:
     assert path.suffix == ".csv"
     df = pd.read_csv(path)
     return df
 
-def select_feature_columns(df:typing.Union[pd.DataFrame|pd.Series]) -> typing.Union[pd.DataFrame|pd.Series]:
-    cols = ["patient_id", "study_id", "patient_age", "psa", "psad", "prostate_volume", "case_csPCa"]
+
+def select_feature_columns(
+    df: typing.Union[pd.DataFrame | pd.Series]
+) -> typing.Union[pd.DataFrame | pd.Series]:
+    cols = [
+        "patient_id",
+        "study_id",
+        "patient_age",
+        "psa",
+        "psad",
+        "prostate_volume",
+        "case_csPCa",
+    ]
     id_features_df = df[cols]
     del df
     gc.collect()
     return id_features_df
 
+
 def calculate_psad(row):
     if pd.isna(row.psad):
-        value = np.round(row.psa/row.prostate_volume, decimals=2)
+        value = np.round(row.psa / row.prostate_volume, decimals=2)
     else:
         value = row.psad
     return value
 
-def fill_psad(df:typing.Union[pd.DataFrame|pd.Series]) -> None:
+
+def fill_psad(df: typing.Union[pd.DataFrame | pd.Series]) -> None:
     df.dropna(axis=0, how="any", subset=["psa", "prostate_volume"], inplace=True)
     df["psad"] = df.apply(calculate_psad, axis=1)
 
-def load_data(path:pathlib.Path) -> typing.Union[pd.DataFrame|pd.Series]:
+
+def load_data(path: pathlib.Path) -> typing.Union[pd.DataFrame | pd.Series]:
     df = load_tabular(path)
     df = select_feature_columns(df)
     fill_psad(df)
     return df
+
 
 # def get_splits(splits_path: pathlib.Path) -> list[dict]:
 #     with open(splits_path, mode="r") as sf:
@@ -75,6 +87,7 @@ def load_data(path:pathlib.Path) -> typing.Union[pd.DataFrame|pd.Series]:
 #         val_fold_dict.update({i: filtered_df_idx})
 #     return train_fold_dict, val_fold_dict
 
+
 def train_ridge_ensemble(data):
     kfold = KFold(n_splits=5)
     models = []
@@ -100,17 +113,20 @@ def train_ridge_ensemble(data):
         os.makedirs("tabular_models", exist_ok=True)
         with open(f"tabular_models/ridge_fold-{fold}.pkl", mode="wb") as model_file:
             pickle.dump(model, model_file)
-    
+
     with open("tabular_models/ridge_scaler.pkl", mode="wb") as scaler_file:
         pickle.dump(scaler, scaler_file)
 
     y_test_pred_ = np.empty((len(models), len(y_test)))
     for i, (_, model) in enumerate(models):
-        y_test_pred_[i] = model.predict(X_test) 
-    y_test_pred = list(map(lambda y_: 1 if y_ > len(models)//2 else 0, y_test_pred_.sum(axis=0)))
+        y_test_pred_[i] = model.predict(X_test)
+    y_test_pred = list(
+        map(lambda y_: 1 if y_ > len(models) // 2 else 0, y_test_pred_.sum(axis=0))
+    )
     roc_auc = roc_auc_score(y_test, y_test_pred)
     f1 = f1_score(y_test, y_test_pred)
     print(f"Ridge ensemble: ROC-AUC: {roc_auc}, F1-score: {f1}")
+
 
 def train_logistic_ensemble(data):
     kfold = KFold(n_splits=5)
@@ -137,17 +153,20 @@ def train_logistic_ensemble(data):
         os.makedirs("tabular_models", exist_ok=True)
         with open(f"tabular_models/logistic_fold-{fold}.pkl", mode="wb") as model_file:
             pickle.dump(model, model_file)
-    
+
     with open("tabular_models/logistic_scaler.pkl", mode="wb") as scaler_file:
         pickle.dump(scaler, scaler_file)
 
     y_test_pred_ = np.empty((len(models), len(y_test)))
     for i, (_, model) in enumerate(models):
-        y_test_pred_[i] = model.predict(X_test) 
-    y_test_pred = list(map(lambda y_: 1 if y_ > len(models)//2 else 0, y_test_pred_.sum(axis=0)))
+        y_test_pred_[i] = model.predict(X_test)
+    y_test_pred = list(
+        map(lambda y_: 1 if y_ > len(models) // 2 else 0, y_test_pred_.sum(axis=0))
+    )
     roc_auc = roc_auc_score(y_test, y_test_pred)
     f1 = f1_score(y_test, y_test_pred)
     print(f"Logistic ensemble: ROC-AUC: {roc_auc}, F1-score: {f1}")
+
 
 def train_mlp_ensemble(data):
     kfold = KFold(n_splits=5)
@@ -160,7 +179,13 @@ def train_mlp_ensemble(data):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
     for fold, (train_idx, val_idx) in enumerate(kfold.split(X_train, y_train), start=1):
-        model = MLPClassifier(hidden_layer_sizes=(8), learning_rate="adaptive", max_iter=200, early_stopping=True, tol=0.1)
+        model = MLPClassifier(
+            hidden_layer_sizes=(8),
+            learning_rate="adaptive",
+            max_iter=200,
+            early_stopping=True,
+            tol=0.1,
+        )
         X_train_ = X_train[train_idx]
         y_train_ = y_train[train_idx]
         model.fit(X_train_, y_train_)
@@ -174,17 +199,20 @@ def train_mlp_ensemble(data):
         os.makedirs("tabular_models", exist_ok=True)
         with open(f"tabular_models/MLP_fold-{fold}.pkl", mode="wb") as model_file:
             pickle.dump(model, model_file)
-    
+
     with open("tabular_models/mlp_scaler.pkl", mode="wb") as scaler_file:
         pickle.dump(scaler, scaler_file)
 
     y_test_pred_ = np.empty((len(models), len(y_test)))
     for i, (_, model) in enumerate(models):
-        y_test_pred_[i] = model.predict(X_test) 
-    y_test_pred = list(map(lambda y_: 1 if y_ > len(models)//2 else 0, y_test_pred_.sum(axis=0)))
+        y_test_pred_[i] = model.predict(X_test)
+    y_test_pred = list(
+        map(lambda y_: 1 if y_ > len(models) // 2 else 0, y_test_pred_.sum(axis=0))
+    )
     roc_auc = roc_auc_score(y_test, y_test_pred)
     f1 = f1_score(y_test, y_test_pred)
     print(f"MLP ensemble: ROC-AUC: {roc_auc}, F1-score: {f1}")
+
 
 def train_svc_ensemble(data):
     kfold = KFold(n_splits=5)
@@ -197,7 +225,7 @@ def train_svc_ensemble(data):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
     for fold, (train_idx, val_idx) in enumerate(kfold.split(X_train, y_train), start=1):
-        model = SVC()
+        model = SVC(probability=True)
         X_train_ = X_train[train_idx]
         y_train_ = y_train[train_idx]
         model.fit(X_train_, y_train_)
@@ -214,20 +242,26 @@ def train_svc_ensemble(data):
 
     with open("tabular_models/svc_scaler.pkl", mode="wb") as scaler_file:
         pickle.dump(scaler, scaler_file)
-    
+
     y_test_pred_ = np.empty((len(models), len(y_test)))
     for i, (_, model) in enumerate(models):
-        y_test_pred_[i] = model.predict(X_test) 
-    y_test_pred = list(map(lambda y_: 1 if y_ > len(models)//2 else 0, y_test_pred_.sum(axis=0)))
+        y_test_pred_[i] = model.predict(X_test)
+    y_test_pred = list(
+        map(lambda y_: 1 if y_ > len(models) // 2 else 0, y_test_pred_.sum(axis=0))
+    )
     roc_auc = roc_auc_score(y_test, y_test_pred)
     f1 = f1_score(y_test, y_test_pred)
     print(f"SVC ensemble: ROC-AUC: {roc_auc}, F1-score: {f1}")
 
-def main(input: pathlib.Path, output:pathlib.Path) -> None:
+
+def main(input: pathlib.Path, output: pathlib.Path) -> None:
     raise NotImplementedError()
 
+
 if __name__ == "__main__":
-    file_path = pathlib.Path("/home/rolandnemeth/AIMI_project/input/picai_labels/clinical_information/marksheet.csv")
+    file_path = pathlib.Path(
+        "/home/rolandnemeth/AIMI_project/input/picai_labels/clinical_information/marksheet.csv"
+    )
     df = load_data(file_path)
     train_ridge_ensemble(df)
     train_logistic_ensemble(df)
--- code/test_tabular_output.py
+++ code/test_tabular_output.py
@@ -1,18 +1,20 @@
 import json
 import pickle
-from pprint import pprint
 from pathlib import Path
 import os
 
 import numpy as np
 import pandas as pd
 
-TEST_TABULAR_PATH = Path("/home/rolandnemeth/AIMI_project/repos/picai_unet_semi_supervised_gc_algorithm/test/clinical-information-prostate-mri.json")
+TEST_TABULAR_PATH = Path(
+    "/home/rolandnemeth/AIMI_project/repos/picai_unet_semi_supervised_gc_algorithm/test/clinical-information-prostate-mri.json"
+)
 
 MODELS_DIR = Path("/home/rolandnemeth/AIMI_project/tabular_models")
 
 os.makedirs("output", exist_ok=True)
 OUTPUT_DIR = Path("/home/rolandnemeth/AIMI_project/output")
+
 
 def main():
     models = []
@@ -31,10 +33,18 @@ def main():
     psad = tabular_data["PSAD_report"]
     prostate_volume = tabular_data["prostate_volume_report"]
 
-    tabular_data_dict = {"patient_age": age, "psa": psa, "psad": psad, "prostate_volume": prostate_volume}
+    tabular_data_dict = {
+        "patient_age": age,
+        "psa": psa,
+        "psad": psad,
+        "prostate_volume": prostate_volume,
+    }
 
-    tabular_data_df = pd.DataFrame(tabular_data_dict, index=[0]) if isinstance(age, (int, float)) else pd.DataFrame.from_dict(tabular_data_dict)
-
+    tabular_data_df = (
+        pd.DataFrame(tabular_data_dict, index=[0])
+        if isinstance(age, (int, float))
+        else pd.DataFrame.from_dict(tabular_data_dict)
+    )
 
     input_data = tabular_data_df
 
@@ -42,10 +52,8 @@ def main():
 
     ensemble_output = np.empty((len(models), len(input_data)))
 
-
     for i, model in enumerate(models):
         ensemble_output[i] = model.predict_proba(input_data)[:, 1]
-        
 
     ensemble_output_mean = ensemble_output.mean(axis=0)
     print(float(ensemble_output_mean))
